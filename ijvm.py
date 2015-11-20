67f737b995f6d2a1c15077ed90c02746d27ea6f5
yurippe <yurippenet@gmail.com>
2015-11-20 11:02:05 +0100

    now it is up and running with minor flaws

--- ijvm.py
+++ ijvm.py
@@ -7,8 +7,13 @@ class Stack(object):
     def setStack(self, l):
         self.stack = l
 
+    def getSize(self):
+        return len(self.stack)
+
+    def pop(self, sp):
+        self.stack.pop()
+
     def __getitem__(self, key):
-        #return str(key) + "works"
         return self.stack[key]
 
     def __setitem__(self, key, value):
@@ -23,22 +28,38 @@ class ConstantPool(object):
         self.constantpool = []
 
     def setConstantPool(self, l):
-        self.constantpool = l
+        for li in l:
+            if type(li) == str:
+                self.constantpool.append(int(li, base=16))
+            elif type(li) == int:
+                self.constantpool.append(li)
+        #self.constantpool = l
+
+    def getSize(self):
+        return len(self.constantpool)
         
     def __getitem__(self, key):
-        if type(key) == int: return key
-        else: return int(self.constantpool[key], base=16)
+        if type(key) == int: return self.constantpool[key]
+        else: return self.constantpool[int(key, base=16)]
 
 class MethodArea(object):
     def __init__(self):
         self.methodarea = []
         
     def setMethodArea(self, l):
-        self.methodarea = l
+        for li in l:
+            if type(li) == str:
+                self.methodarea.append(int(li, base=16))
+            elif type(li) == int:
+                self.methodarea.append(li)
+        #self.methodarea = l
+
+    def getSize(self):
+        return len(self.methodarea)
 
     def __getitem__(self, key):
-        if type(key) == int: return key
-        else: return int(self.constantpool[key], base=16)
+        if type(key) == int: return self.methodarea[key]
+        else: return self.methodarea[int(key, base=16)]
 
 
         
@@ -47,34 +68,44 @@ class IJVM(object):
     
     def __init__(self, image):
         self.image = image
-        
-        self.sp = 0           
-        self.lv = 0                 
-        self.pc = INITIAL_PC
-        self.wide = False
 
         self.stack = Stack()
         self.cpp = ConstantPool()
         self.method = MethodArea()
 
-        self.stack.setStack([])
-        self.cpp.setConstantPool(image.getConstantPool())
+        
         self.method.setMethodArea(image.getMethodArea())
+        self.cpp.setConstantPool(image.getConstantPool())
+        self.stack.setStack([])
+        
+        self.sp = self.stack.getSize() - 1          
+        self.lv = 0                 
+        self.pc = INITIAL_PC
+        self.wide = False
+
+
 
         self.initial_sp = self.sp;
 
         #Set everything up
         self.push(INITIAL_OBJ_REF)
         #Push all arguments here
-
         
         self.invoke_virtual(self.image.getMainIndex())
-
-        print self.stack.stack
+        
     def start(self):
-        while self.active:
+        print "Stack: " + str(self.stack.stack)
+        while self.active():
             self.execute_opcode()
-            print self.stack.stack
+            tmp = [str(x) for x in self.stack.stack]
+            try:
+                tmp[self.sp] = "SP->" + tmp[self.sp]
+                tmp[self.lv] = "LV->" + tmp[self.lv]
+            except: pass
+            print "-----------"
+            print "Stack: " + str(self.stack.stack)
+            print tmp
+            print "-----------"
 
         self.print_result()
 
@@ -102,6 +133,7 @@ class IJVM(object):
 
     def pop(self):
         result = self.stack[self.sp]
+        self.stack.pop(self.sp)
         self.sp = self.sp - 1
         return result
 
@@ -113,7 +145,6 @@ class IJVM(object):
         address = self.cpp[index]
         nargs = self.method[address] * 256 + self.method[address + 1]
         nlocals = self.method[address + 2] * 256 + self.method[address + 3]
-        
         self.sp = self.sp + nlocals
         self.push(self.pc)
         self.push(self.lv)
@@ -122,7 +153,6 @@ class IJVM(object):
         self.pc = address + 4
 
     def ireturn(self):
-
         linkptr = self.stack[self.lv]
         self.stack[self.lv] = self.stack[self.sp]
         self.sp = self.lv
@@ -133,6 +163,9 @@ class IJVM(object):
 
         opc = self.pc
         opcode = self.fetchByte()
+
+        print "PC:" + str(opc) + "; OPCODE: [" + str(hex(opcode)) + "]"
+
 
         if opcode == OPCODE_BIPUSH:
             self.push(self.fetchByte(signed=True))
@@ -241,10 +274,17 @@ class IJVM(object):
 if __name__ == "__main__":
 
     image = IJVMImage()
-    image.method_area = [0x00,0x01,0x00,0x00,0x10,0x01,0x10,0x02,0x60,0x10,0x04,0x60,0x10,0x03,0x60]
-    image.constant_pool = [0x00000000]
-
-    print image.method_area
+    image.method_area = ['0x00', '0x03', '0x00', '0x01', '0x10', '0x00', '0x36', '0x03', '0x15', '0x01', '0x9b', '0x00', '0x19', '0x15', '0x01', '0x99', '0x00', '0x14', '0x15', '0x01', '0x10', '0x01', '0x64', '0x36', '0x01', '0x15', '0x03', '0x15', '0x02', '0x60', '0x36', '0x03', '0xa7', '0xff', '0xe8', '0x15', '0x03', '0xac', '0x00', '0x01', '0x00', '0x00', '0x10', '0x2c', '0x10', '0x0a', '0x10', '0x49', '0xb6', '0x00', '0x00', '0xac']
+    image.constant_pool = [0x00000000, 0x26]
+    image.main_index = 1
+    i = 0
+    tmp = []
+    print "Method area"
+    for x in image.method_area:
+        tmp.append(str(i) + ": " + x)
+        i += 1
+    print tmp
+    print "Constant pool"
     print image.constant_pool
     ijvm = IJVM(image)
 
